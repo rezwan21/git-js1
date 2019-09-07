5cd157495e8c606a186e3fd9243e8c653c48b27c
rezwan21 <mrezwan451@gmail.com>
2019-09-07 12:34:59 -0600

    added a new method called display to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello{
     public static void main(string[] args){
         system.out.println("VS code is awesome");
+        greeting("rezwan21");
+    }
+    public static void greeting(String name){
+        system.out.println("Welcome"+name);
     }
 }
